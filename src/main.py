a0accd2685ecbf53126b64e7cb2849d56831f820
Xing Jiliang <1458103052@qq.com>
2019-08-22 16:04:25 +0800

    测试

--- src/main.py
+++ src/main.py
@@ -15,6 +15,7 @@ def main(_):
     global_config = config.Configuration(FLAGS)
     # print(global_config)
     train.train(global_config)
+    global_config.batch_size = 65536
     test.test(global_config)
 
 
